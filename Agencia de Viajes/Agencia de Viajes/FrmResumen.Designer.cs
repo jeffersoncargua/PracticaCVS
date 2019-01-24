9015a8c9a6bbf42e02c3a72257c2196ab3df9b8e
MARLON PAUL APOLO QUISHPE <marlon.apolo@epn.edu.ec>
2019-01-24 16:54:55 -0500

    Cambio de colores y letra en el formulario base y en el botón salir

--- Agencia de Viajes/Agencia de Viajes/FrmResumen.Designer.cs
+++ Agencia de Viajes/Agencia de Viajes/FrmResumen.Designer.cs
@@ -31,16 +31,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lbxVendidos = new System.Windows.Forms.ListBox();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(120, 47);
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(238, 32);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.Size = new System.Drawing.Size(290, 35);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tickets Vendidos";
             // 
@@ -49,41 +49,34 @@
             this.lbxVendidos.FormattingEnabled = true;
             this.lbxVendidos.ItemHeight = 16;
             this.lbxVendidos.Location = new System.Drawing.Point(49, 87);
-            this.lbxVendidos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lbxVendidos.Margin = new System.Windows.Forms.Padding(4);
             this.lbxVendidos.Name = "lbxVendidos";
             this.lbxVendidos.Size = new System.Drawing.Size(633, 116);
             this.lbxVendidos.TabIndex = 1;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(124, 226);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalir.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnSalir.Font = new System.Drawing.Font("Perpetua Titling MT", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.Location = new System.Drawing.Point(305, 222);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(100, 28);
+            this.btnSalir.Size = new System.Drawing.Size(126, 42);
             this.btnSalir.TabIndex = 2;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(494, 249);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(188, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "GRACIAS POR SU COMPRA";
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
             // 
             // FrmResumen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
             this.ClientSize = new System.Drawing.Size(756, 290);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.lbxVendidos);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmResumen";
             this.Text = "FrmResumen";
             this.Load += new System.EventHandler(this.FrmResumen_Load);
@@ -97,6 +90,5 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lbxVendidos;
         private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.Label label2;
     }
 }
