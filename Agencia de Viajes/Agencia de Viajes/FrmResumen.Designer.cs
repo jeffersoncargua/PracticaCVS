96973b496f4164f541818191c625288618c96eee
MARLON PAUL APOLO QUISHPE <marlon.apolo@epn.edu.ec>
2019-01-23 23:46:33 -0500

    Prueba para modificacion de Formulario

--- Agencia de Viajes/Agencia de Viajes/FrmResumen.Designer.cs
+++ Agencia de Viajes/Agencia de Viajes/FrmResumen.Designer.cs
@@ -31,42 +31,59 @@
             this.label1 = new System.Windows.Forms.Label();
             this.lbxVendidos = new System.Windows.Forms.ListBox();
             this.btnSalir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 38);
+            this.label1.Location = new System.Drawing.Point(120, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.Size = new System.Drawing.Size(116, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tickets Vendidos";
             // 
             // lbxVendidos
             // 
             this.lbxVendidos.FormattingEnabled = true;
-            this.lbxVendidos.Location = new System.Drawing.Point(37, 71);
+            this.lbxVendidos.ItemHeight = 16;
+            this.lbxVendidos.Location = new System.Drawing.Point(49, 87);
+            this.lbxVendidos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lbxVendidos.Name = "lbxVendidos";
-            this.lbxVendidos.Size = new System.Drawing.Size(476, 95);
+            this.lbxVendidos.Size = new System.Drawing.Size(633, 116);
             this.lbxVendidos.TabIndex = 1;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(93, 184);
+            this.btnSalir.Location = new System.Drawing.Point(124, 226);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(100, 28);
             this.btnSalir.TabIndex = 2;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(494, 249);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GRACIAS POR SU COMPRA";
+            // 
             // FrmResumen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 236);
+            this.ClientSize = new System.Drawing.Size(756, 290);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.lbxVendidos);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmResumen";
             this.Text = "FrmResumen";
             this.Load += new System.EventHandler(this.FrmResumen_Load);
@@ -80,5 +97,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lbxVendidos;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label2;
     }
 }
